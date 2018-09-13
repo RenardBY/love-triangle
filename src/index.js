9e44d6f20f7cea69f64e802ef20fbdcd9bde0caa
Andrei Poleschuk <renardby@mail.ru>
2018-09-13 15:58:59 +0300

    second task

--- src/index.js
+++ src/index.js
@@ -3,5 +3,43 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+
+  let triangles = 0;
+
+  preferences.forEach(function callback(currentValue, index, array) {
+
+    let firstNode = array[index];
+    let secondNode = array[firstNode-1];
+    let thirdNode = array[secondNode-1];
+
+    function clearNode() {
+      firstNode = undefined;
+      secondNode = undefined;
+      thirdNode = undefined;
+    }
+
+    function clearCurrentValue(){
+      array[index] = undefined;
+      array[firstNode-1] = undefined;
+      array[secondNode-1] = undefined;
+    }
+
+    if (firstNode-1 == index) {
+      array[index] = undefined;
+      clearNode();
+    }
+
+    if (firstNode == secondNode) {
+       clearCurrentValue();
+       clearNode();
+    }
+
+    if (index+1 == thirdNode) {
+      triangles++;
+      clearCurrentValue()
+    }
+
+  }
+);
+return triangles;
 };
